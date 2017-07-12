dea5fb8001a2a31fdb35bdc3e3735d594f013596
Esteban Herrera <eh@MacBook-Pro-de-Esteban.local>
2017-07-12 09:59:04 -0500

    first commit

---
+++ ARAnimation/ViewController.swift
@@ -0,0 +1,154 @@
+//
+//  ViewController.swift
+//  ARAnimation
+//
+//  Created by Esteban Herrera on 7/11/17.
+//  Copyright © 2017 Esteban Herrera. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    var animations = [String: CAAnimation]()
+    var idle:Bool = true;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        // Load the DAE animations
+        loadAnimations();
+    }
+    
+    func loadAnimations () {
+        // Load the character in the idle animation
+        let idleScene = SCNScene(named: "art.scnassets/idleFixed.dae")!
+        
+        // This node will be parent of all the animation models
+        let node = SCNNode()
+        
+        // Add all the child nodes to the parent node
+        for child in idleScene.rootNode.childNodes {
+            node.addChildNode(child)
+        }
+        
+        // Set up some properties
+        node.position = SCNVector3(0, -1, -2)
+        node.scale = SCNVector3(0.2, 0.2, 0.2)
+        
+        // Add the node to the scene
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        // Load all the DAE animations
+        loadAnimation(withKey: "dancing", sceneName: "art.scnassets/twist_danceFixed", animationIdentifier: "twist_danceFixed-1")
+    }
+    
+    
+    func loadAnimation(withKey: String, sceneName:String, animationIdentifier:String) {
+        let sceneURL = Bundle.main.url(forResource: sceneName, withExtension: "dae");
+        let sceneSource = SCNSceneSource(url: sceneURL!, options: nil);
+        let animationObject = sceneSource?.entryWithIdentifier(animationIdentifier, withClass: CAAnimation.self)
+        
+        // The animation will only play once
+        animationObject!.repeatCount = 1
+        // To create smooth transitions between animations
+        animationObject!.fadeInDuration = CGFloat(1)
+        animationObject!.fadeOutDuration = CGFloat(0.5)
+        
+        // Store the animation for later use
+        animations[withKey] = animationObject!
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: sceneView)
+        
+        // Let's test if a 3D Object was touch
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        
+        let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+        
+        if hitResults.first != nil {
+            if(idle) {
+                playAnimation(key: "dancing");
+            } else {
+                stopAnimation(key: "dancing");
+            }
+            idle = !idle
+            return
+        }
+    }
+    
+    func playAnimation(key: String) {
+        // Add the animation to start playing it right away
+        sceneView.scene.rootNode.addAnimation(animations[key]!, forKey: key);
+    }
+    
+    func stopAnimation(key: String) {
+        // Stop the animation with a smooth transition
+        sceneView.scene.rootNode.removeAnimation(forKey: key, blendOutDuration: CGFloat(0.5))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingSessionConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# ARKitAnimation
+Simple animation of a 3D model made with Mixamo and presented in an AR scene with ARKit. Follow the tutorial at the Pusher blog (not yet published).
+
+[![Animating a 3D model with ARKit and Mixamo](https://img.youtube.com/vi/MLBQWCgRepk/0.jpg)](http://www.youtube.com/watch?v=MLBQWCgRepk)
+
+
+# Requirements
+
+- Xcode 9 (Beta 2 at least)
+- iOS 11
+- An iOS device with an A9 or better processor (iPhone 6s or superior, iPad Pro, iPad 2017)
+
+# Installation
+1. Clone this repository and `cd` into it.
+2. Open the project in Xcode 9.
+3. You have to run the project in a real device. AR won’t work in the simulator, so configure a team to sign your app (a paid developer account is not required for testing in one device).
+
+# Credits
+The following 3D model was used:
+- [Obese Male](https://free3d.com/3d-model/obese-male-71456.html)
+
+# License
+MIT
